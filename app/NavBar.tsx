c7c2f442f98e504d9dee530037d1cecd659b19aa
nainsworth <nja8161995@gmail.com>
2025-03-10 20:56:52 -0500

    Styled active navbar link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { GiAlienBug } from "react-icons/gi";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -17,7 +23,11 @@ const NavBar = () => {
           <Link
             key={link.label}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
