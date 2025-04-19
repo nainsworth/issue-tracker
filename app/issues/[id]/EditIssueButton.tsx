2f51d50b90e755425dfa7f8f3e148eaac33485ac
nainsworth <nja8161995@gmail.com>
2025-04-19 16:49:59 -0500

    Refactor: Apply SRP

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,19 @@
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+
+const EditIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <Button>
+      <Link
+        href={`/issues/${issueId}/edit`}
+        className="flex items-center gap-1"
+      >
+        <Pencil2Icon />
+        Edit Issue
+      </Link>
+    </Button>
+  );
+};
+
+export default EditIssueButton;
